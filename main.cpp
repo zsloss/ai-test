47f8887e2f381557567211994e48ea0ac78a7137
Zachary Sloss <zachary.sloss@gmail.com>
2014-11-13 18:30:22 +0800

    Random number function added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <random>
 
 using namespace std;
 
@@ -10,11 +11,19 @@ string int_to_string(int x) {
     return ss.str();
 }
 
+int rnd_gen(int min, int max) {
+    static random_device rd;
+    static default_random_engine generator(rd());
+    uniform_int_distribution<int> uni_dist(min, max);
+    return uni_dist(generator);
+}
+
 class Person {
     public:
         int hunger;
         string name;
         Person();
+        Person(string);
     private:
         static int people;
         int id;
@@ -27,8 +36,15 @@ Person::Person() {
     name = "Person " + int_to_string(id);
 }
 
+Person::Person(string n) {
+    Person();
+    name = n;
+}
+
 int main(int argc, char* argv[]) {
     cout << "Hello, idiots!" << endl;
-    Person p = Person();
-    cout << p.name << endl;
+    Person p = Person("Twatmongrel");
+   cout << rnd_gen(1, 6) << endl;
 }
+
+
